8249bf50486413a1f1b478103ec8c7a8ba839c72
Brent Colson <bacolson@gmail.com>
2016-06-09 20:32:59 -0400

    Added before method for clearing db and adding fresh

--- specs/server/ServerSpec.js
+++ specs/server/ServerSpec.js
@@ -3,11 +3,44 @@ var request = require('request');
 
 var app = require('../../server/server.js');
 var Restaurant = require('../../server/db/models.js');
-var port = process.env.PORT || 3000;
-
 
 
 describe('/rest - Restaurants', function () {
+
+  before(function (done) {
+    Restaurant.find({}).then(function (docs) {
+      docs.forEach(function (doc) {
+        doc.remove().then(function () {
+        });
+      });
+    }).catch(function (err) {
+      done(err);
+      throw {
+        type: 'DatabaseError',
+        message: 'Failed to clear Restaurant Colletion'
+      };
+    }).then(function () {
+      new Restaurant({
+        restaurantName: String,
+        restaurantAddress: "93 School St, Watertown, MA",
+        priceRating: 10,
+        serviceRating: 10,
+        foodRating: 10,
+        ambienceRating: 10,
+        restaurantReview: "Best buttermilk-dutch-oven fried chicken on the planet.",
+        userEmail: 'bc@gmail.com',
+        image: "http://s3-media1.fl.yelpcdn.com/bphoto/YiLAieTSO18oPjEuRzZSQA/348s.jpg"
+      }).save().then(function(){
+        done();
+      }).catch(function (err) {
+        done(err);
+        throw {
+          type: 'DatabaseError',
+          message: 'Failed to save new Product'
+        };
+      });
+    });
+  });
 
   it('Retrieve all ', function (done) {
 
