4dae7bb17339d90d9c736abcc3ac1c9a09c416a2
S.B. Lee <echo3042@gmail.com>
2016-06-10 12:08:26 -0700

    (Feat) Setting up Client Side Test with Karma and Server Side with Mocha

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'specs/server/*.js'
+      'specs/client/*.js',
+      'client/**/*.*'
     ],
 
 
